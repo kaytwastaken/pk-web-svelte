2e95b1e61a0274cee3d4e9d334466cc25b38d1b9
Airrocket <airrocket.buksas@gmail.com>
2022-05-06 23:14:51 -0600

    bump displayed version

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -8,7 +8,7 @@
 <footer>
     <span>
         <a href="https://kayt.dev" class="external" target="_blank">kayt.dev</a>
-        <p class="gray">v0.3.1</p>
+        <p class="gray">v0.3.2</p>
     </span>
 </footer>
 
